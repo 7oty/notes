43eaca0c66dfa4cc2cacf3593901e97ff2fce886
leeseaungx <Lseaungx@163.com>
2016-11-20 18:23:33 +0800

    about the notes write

---
+++ About_javascript/base.js
@@ -0,0 +1,11 @@
+//0x00 base type
+
+
+function Type(){
+    var name = "leeseaung";//string type
+    var age = 20;//int type(Number)
+    var score = 99.8;//float type(Number)
+    console.log("hi! my name is "+ name);
+    console.log("my age " + age);
+    console.log("my score" + score);
+}
