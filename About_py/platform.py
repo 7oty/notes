c909e96f069278618a399fb307d700ddef5b63b6
leeseaungx <Lseaungx@163.com>
2016-12-10 22:21:54 +0800

    platform moudle

---
+++ About_py/platform.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import platform
+
+print(platform.platform())
+print(platform.system())
+ptint(platform.uname())
+print(platform.node())
+print(platform.version())
+print(platform.machine())
+print(platform.python_version())
