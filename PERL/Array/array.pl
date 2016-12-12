d0f28f9c9a29ccd743ea6c2b16eeccc8522166cb
leeseaungx <Lseaungx@163.com>
2016-12-12 22:40:37 +0800

    perl

---
+++ PERL/Array/array.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+@array = (1, 3, "hello"); #create array
+@array = qw/monday
+Tuesday
+sunday/;
+
+@var_10 = (1..10);
+@var_20 = (10..20);
+@var_abc = (a..z);
+
+print "@var_10\n";
+print "@var_20\n";
+print "@var_abc\n";
---
+++ PERL/Variable/var.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+$age = 25;
+$name = "seaungx";
+
+print "age = $age\n";
+print "name = $name\n";
