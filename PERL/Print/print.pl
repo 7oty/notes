1f69da52f078d9f7a8988ae8410522401d628356
leeseaungx <Lseaungx@163.com>
2016-12-14 23:47:25 +0800

    perl programming

---
+++ PERL/Print/print.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+print "hello world!\n";
+print "the answer is";
+print 6.*7;
+print "\n";
---
+++ PERL/control/if.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+$a = 10;
+if($a < 20){
+    printf "a is less than 20\n";
+}
+
+print "value of a is : $a\n";
+
+$a = "";
+
+if($a){
+    printf "a has a true value\n";
+}
+
+print "value of a is : $a\n";
