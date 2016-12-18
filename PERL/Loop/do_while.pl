1206890efcfc1d8af94f07478bc9c440eaa09316
leeseaungx <Lseaungx@163.com>
2016-12-18 13:05:13 +0800

    perl loop

---
+++ PERL/Loop/do_while.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+$a = 10;
+
+do{
+    printf "value of a : $a\n";
+    $a = $a + 1;
+}while($a < 20);
---
+++ PERL/Loop/while.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+$a = 10;
+
+while($a < 20){
+    printf "value of a : $a\n";
+    $a = $a + 1;
+}
