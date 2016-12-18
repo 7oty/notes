de9341e392ce082384d0af8c4c793c1d394b29e4
leeseaungx <Lseaungx@163.com>
2016-12-18 14:04:57 +0800

    perl variable

---
+++ PERL/Variable/hash_var.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+%data = ("job", 45, "lisa", 46, "top", 47);
+print "\$data{'job'} = $data{'job'}\n";
+print "\$data{'lisa'} = $data{'lisa'}\n";
+print "\$data{'top'} = $data{'top'}\n";
---
+++ PERL/Variable/scalar_var.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+$age = 25;
+$name = "seaung";
+$salary = "1445.50";
+
+print "age = $age\n";
+print "name = $name\n";
+print "salary = $salary\n";
